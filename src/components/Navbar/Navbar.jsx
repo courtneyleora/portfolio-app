b210f08ff5c17004a1153816d8f0f2c5ab6b88bc
Courtney Ross <cross52@student.gsu.edu>
2025-08-28 14:06:18 -0400

    Add Navbar component with navigation links and logo

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./Navbar.css/";
+import logo from "../../assets/lgt_logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img id="logo" src={logo} alt="" />
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect with me!</div>
+    </div>
+  );
+};
+
+export default Navbar;
