f5e743172a92ee9f1fef908aef49d65283309bd5
Courtney Ross <cross52@student.gsu.edu>
2025-08-28 14:06:41 -0400

    Set up initial HTML structure, import Navbar and Hero components, and add global styles

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+    return (
+        <div>
+          <Navbar/>
+          <Hero/>
+        </div>
+    );
+}
+export default App;
