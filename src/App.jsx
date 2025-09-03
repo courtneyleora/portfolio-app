a1746f911f7f686b118b90ba11fda3c4a3f30e59
Courtney Ross <cross52@student.gsu.edu>
2025-09-03 19:25:21 -0400

    Add Service component to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Service from "./components/Service/Service";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Service />
     </div>
   );
 };
