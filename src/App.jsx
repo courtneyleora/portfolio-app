0d249695a3b0160ca53c37514ebaec0e12f370e2
Courtney Ross <cross52@student.gsu.edu>
2025-09-09 20:25:41 -0400

    Add Portfolio component to App for enhanced project showcase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Portfolio from "./components/Portfolio/Portfolio";
 import Service from "./components/Service/Service";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <About />
       <Service />
+      <Portfolio />
     </div>
   );
 };
